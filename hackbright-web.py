70a2442d32a5cfa0223da1c62d83edfa018387d2
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-18 17:16:40 -0700

    Created /student-search and /add-student routes to show forms for student searches and student creation; created corresponding templates

--- hackbright-web.py
+++ hackbright-web.py
@@ -9,10 +9,61 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github', 'jhacks')
     first, last, github = hackbright.get_student_by_github(github)
-    return "%s is the GitHub account for %s %s" % (github, first, last)
+    html = render_template("student_info.html",
+                           first=first,
+                           last=last,
+                           github=github)
+    return html
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+
+@app.route("/add-student", methods=['POST'])
+def student_add():
+    """Add a student."""
+
+    first = request.form.get('first')
+    last = request.form.get('last')
+    github = request.form.get('github')
+
+
+    hackbright.make_new_student(first, last, github)
+
+    return render_template("student_added.html",
+                           github=github)
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
     app.run(debug=True)
+
+
+
+
+
+
+
+# @app.route("/student-search")
+# def get_student_form(github=None):
+#     """Show form for searching for a student."""
+
+#     return render_template("student_search.html", 
+#                             github=github)
+
+
+# @app.route("/student-add", methods=['POST'])
+# def student_add():
+#     """Add a student."""
+
+#     github = request.form.get('github')
+
+#     get_student_form(github)
+
+#     # render_template("student_added.html",
+#                            # github=github)
